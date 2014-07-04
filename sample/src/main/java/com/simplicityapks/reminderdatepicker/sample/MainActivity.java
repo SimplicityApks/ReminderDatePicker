5139a353d315839402fec865d74062aa91efc977
SimplicityApks <simplicityapks@gmail.com>
2014-07-04 14:37:34 +0200

    Update sample with listener+Toast

--- sample/src/main/java/com/simplicityapks/reminderdatepicker/sample/MainActivity.java
+++ sample/src/main/java/com/simplicityapks/reminderdatepicker/sample/MainActivity.java
@@ -4,16 +4,39 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.simplicityapks.reminderdatepicker.lib.OnDateSelectedListener;
+import com.simplicityapks.reminderdatepicker.lib.ReminderDatePicker;
+
+import java.util.Calendar;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private ReminderDatePicker datePicker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        datePicker = (ReminderDatePicker) findViewById(R.id.date_picker);
+
+        // setup listener for a date change:
+        datePicker.setOnDateSelectedListener(new OnDateSelectedListener() {
+            @Override
+            public void onDateSelected(Calendar date) {
+                Toast.makeText(MainActivity.this, "Selected date: "+ getDateFormat().format(date), Toast.LENGTH_SHORT);
+            }
+        });
     }
 
+    private java.text.DateFormat savedFormat;
+    public java.text.DateFormat getDateFormat() {
+        if(savedFormat == null)
+            savedFormat = android.text.format.DateFormat.getDateFormat(getApplicationContext());
+        return savedFormat;
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
