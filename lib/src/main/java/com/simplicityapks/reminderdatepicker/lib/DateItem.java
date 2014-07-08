412d4cffb8bc20928263fdcb8d630fe5a3f05578
SimplicityApks <simplicityapks@gmail.com>
2014-07-08 15:53:03 +0200

    Switch DateItem parameters to match GregorianCalender

--- lib/src/main/java/com/simplicityapks/reminderdatepicker/lib/DateItem.java
+++ lib/src/main/java/com/simplicityapks/reminderdatepicker/lib/DateItem.java
@@ -10,7 +10,7 @@ import java.util.GregorianCalendar;
 public class DateItem implements TwinTextItem{
 
     private final String label;
-    private final int day, month, year;
+    private final int year, month, day;
 
     /**
      * Constructs a new DateItem holding the specified date and a label to show primarily.
@@ -18,21 +18,21 @@ public class DateItem implements TwinTextItem{
      * @param date The date to be returned by getDate().
      */
     public DateItem(String label, Calendar date) {
-        this(label, date.get(Calendar.DAY_OF_MONTH), date.get(Calendar.MONTH), date.get(Calendar.YEAR));
+        this(label, date.get(Calendar.YEAR), date.get(Calendar.MONTH), date.get(Calendar.DAY_OF_MONTH));
     }
 
     /**
      * Constructs a new DateItem holding the specified date and a label to to show primarily.
      * @param label The string to return when getPrimaryText() is called.
-     * @param day The day of the month.
-     * @param month The month of year, zero-indexed (so 11 is December).
      * @param year The year.
+     * @param month The month of year, zero-indexed (so 11 is December).
+     * @param day The day of the month.
      */
-    public DateItem(String label, int day, int month, int year) {
+    public DateItem(String label, int year, int month, int day) {
         this.label = label;
-        this.day = day;
-        this.month = month;
         this.year = year;
+        this.month = month;
+        this.day = day;
     }
 
     /**
